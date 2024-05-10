933fa9e64a65c6aedda7029e71d914d1ed44a92d
陈海俊 <353376352@qq.com>
2024-05-10 23:16:53 +0800

    add package: quill

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,13 +12,13 @@ import {ProjectModule} from './project/project.module';
 import {ApiInterceptor} from "./core/interceptors/api.interceptor";
 import {TokenInterceptor} from "./core/interceptors/token.interceptor";
 import {ResponseInterceptor} from "./core/interceptors/response.interceptor";
-import {NZ_I18N} from 'ng-zorro-antd/i18n';
-import {zh_CN} from 'ng-zorro-antd/i18n';
+import {NZ_I18N, zh_CN} from 'ng-zorro-antd/i18n';
 import {registerLocaleData} from '@angular/common';
 import zh from '@angular/common/locales/zh';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {NzMessageModule} from "ng-zorro-antd/message";
 import {NzBackTopModule} from "ng-zorro-antd/back-top";
+import {QuillModule} from "ngx-quill";
 
 registerLocaleData(zh);
 
@@ -26,19 +26,20 @@ registerLocaleData(zh);
   declarations: [
     AppComponent
   ],
-    imports: [
-        BrowserModule,
-        FormsModule,
-        HttpClientModule,
-        RouterModule,
-        AppRoutingModule,
-        UserModule,
-        CoreModule,
-        ProjectModule,
-        BrowserAnimationsModule,
-        NzMessageModule,
-        NzBackTopModule
-    ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule,
+    AppRoutingModule,
+    UserModule,
+    CoreModule,
+    ProjectModule,
+    BrowserAnimationsModule,
+    NzMessageModule,
+    NzBackTopModule,
+    QuillModule.forRoot()
+  ],
   providers: [
     {provide: HTTP_INTERCEPTORS, useClass: ApiInterceptor, multi: true},
     {provide: HTTP_INTERCEPTORS, useClass: TokenInterceptor, multi: true},
